31fea1be30aeaf1995ca1f9b84f57d385e6bd223
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-28 20:16:46 +0530

    Initial Commit

---
+++ diceRoll/diceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceRollApp.swift
+//  diceRoll
+//
+//  Created by Subhrajyoti Chakraborty on 28/09/20.
+//
+
+import SwiftUI
+
+@main
+struct diceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
